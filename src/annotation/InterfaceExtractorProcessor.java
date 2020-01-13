259be579a85481f0aaaee244309722999b31cdd9
DoctorToliet <xuanjinnan@163.com>
2020-01-13 20:41:14 +0800

    .

--- src/annotation/InterfaceExtractorProcessor.java
+++ src/annotation/InterfaceExtractorProcessor.java
@@ -1,5 +1,7 @@
 package annotation;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -7,12 +9,18 @@ import java.util.Collection;
 import com.sun.mirror.apt.AnnotationProcessor;
 import com.sun.mirror.apt.AnnotationProcessorEnvironment;
 import com.sun.mirror.declaration.MethodDeclaration;
+import com.sun.mirror.declaration.ParameterDeclaration;
 import com.sun.mirror.declaration.TypeDeclaration;
 
 public class InterfaceExtractorProcessor implements AnnotationProcessor {
-	private final AnnotationProcessorEnvironment env = null;
+	private final AnnotationProcessorEnvironment env;
 	private ArrayList<MethodDeclaration> interfaceMethods = 
 			new ArrayList<MethodDeclaration>();
+	
+	public InterfaceExtractorProcessor(AnnotationProcessorEnvironment env) {
+		this.env = env;
+	}
+
 	@Override
 	public void process() {
 		Collection<TypeDeclaration> typeDecs = env.getSpecifiedTypeDeclarations();
@@ -27,7 +35,28 @@ public class InterfaceExtractorProcessor implements AnnotationProcessor {
 					interfaceMethods.add(m);
 			}
 			if(interfaceMethods.size() > 0) {
-				env.getf
+				try {
+					PrintWriter writer = env.getFiler().createSourceFile(annot.value());
+					writer.println("package " + typeDec1.getPackage().getQualifiedName() + ";");
+					writer.println("public interface " + annot.value() + " {");
+					for(MethodDeclaration m : methods){
+						writer.print(" public ");
+						writer.print(m.getReturnType() + " ");
+						writer.print(m.getSimpleName() + " (");
+						int i = 0;
+						for(ParameterDeclaration parm : m.getParameters()){
+							writer.print(parm.getType() + " " + parm.getSimpleName());
+							if(i++ < m.getParameters().size())
+								writer.print(", ");
+						}
+						writer.println(");");
+					}
+					writer.println("}");
+					writer.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
 	}
