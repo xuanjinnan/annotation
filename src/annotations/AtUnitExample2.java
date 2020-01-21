f81b23b1f12ce4a856f04a3b9d52c8f7c122d268
280520 <xuanjinnan@163.com>
2020-01-21 09:51:17 +0800

    Revert "AtUnitTest genericity,@TestObjectCreat,@TestProperty,@TestObjectCleanup"
    This reverts commit 5e4a8c48d011679c41351e8db2cadf4baa75cc94.

--- src/annotations/AtUnitExample2.java
+++ src/annotations/AtUnitExample2.java
@@ -1,36 +1,34 @@
-// annotations/AtUnitExample2.java
-// Assertions and exceptions can be used in @Tests
-// {java onjava.atunit.AtUnit
-// build/classes/main/annotations/AtUnitExample2.class}
 package annotations;
+
 import java.io.*;
 import onjava.atunit.*;
 import onjava.*;
+
 public class AtUnitExample2 {
-    public String methodOne() {
-        return "This is methodOne";
-    }
-    public int methodTwo() {
-        System.out.println("This is methodTwo");
-        return 2;
-    }
-    @Test
-    void assertExample() {
-        assert methodOne().equals("This is methodOne");
-    }
-    @Test
-    void assertFailureExample() {
-        assert 1 == 2: "What a surprise!";
-    }
-    @Test
-    void exceptionExample() throws IOException {
-        try(FileInputStream fis =
-                    new FileInputStream("nofile.txt")) {} // Throws
-    }
-    @Test
-    boolean assertAndReturn() {
-        // Assertion with message:
-        assert methodTwo() == 2: "methodTwo must equal 2";
-        return methodOne().equals("This is methodOne");
-    }
+
+	public String methodOne(){
+		return "This is methodOne";
+	}
+	public int methodTwo(){
+		System.out.println("This is methodTwo");
+		return 2;
+	}
+	@Test
+	void assertExample(){
+		assert methodOne().equals("This is methodOne");
+	}
+	@Test
+	void assertFailureExample(){
+		assert 1 == 2 : "What a surprise!";
+	}
+	@Test
+	void exceptionExample() throws IOException{
+		try(FileInputStream fis = new FileInputStream("nofile.text")){}
+	}
+	@Test
+	boolean assertAndReturn(){
+		// Assertion with message:
+		assert methodTwo() == 2 : "methodTwo must equal 2";
+		return methodOne().equals("This is methodOne");
+	}
 }
