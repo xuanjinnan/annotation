19010e5a6e109aed93164e357c341395043705bc
280520 <xuanjinnan@163.com>
2020-01-21 09:56:46 +0800

    Revert "Unit"
    This reverts commit 94abf635a10156dd4d7511bf13ff381a1e1a8f06.

--- src/annotations/DemoProcessFiles.java
+++
@@ -1,9 +0,0 @@
-package annotations;
-
-import onjava.ProcessFiles;
-
-public class DemoProcessFiles {
-	public static void main(String[] args) {
-		new ProcessFiles(file -> System.out.println(file),"java").start(args);
-	}
-}
